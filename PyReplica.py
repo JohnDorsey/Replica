3e56eb5ace4dbed8faa6b71015fb4625f18db757
john <laptopjohn10@gmail.com>
2021-08-18 02:36:36 -0400

    simplify interpreter

--- PyReplica.py
+++ PyReplica.py
@@ -3,31 +3,26 @@ from PyReplicaCompile import formatCompiledLine
 RVM_CONSTANTS = {"INSTRUCTION_DELIMITER":";","ARGUMENT_DELIMITER":"`"}
 
 
+def coreReplace(data,searchTerm,replacementTerm):
+  return (searchTerm in data, data.replace(searchTerm,replacementTerm))
+  
 class ReplicaVirtualMachine:
+
   def __init__(self, programTape, dataTape):
     self.programTape, self.dataTape, self.location = programTape, dataTape, 0
     self.splitProgramTape = self.programTape.split(RVM_CONSTANTS["INSTRUCTION_DELIMITER"])
     
-  def coreReplace(self,searchTerm,replacementTerm, quietly=True):
-    foundAnything = searchTerm in self.dataTape
-    #if not quietly:
-    #  print("coreReplace: "+(found" if foundAnything else "didn't find") + " " + str(searchTerm) + ".")
-    self.dataTape = self.dataTape.replace(searchTerm,replacementTerm)
-    return foundAnything
-    
   def doCycle(self,quietly=True):
     currentWord = self.splitProgramTape[self.location]
     searchTerm, replacementTerm, successJumpText, failureJumpText = currentWord.split(RVM_CONSTANTS["ARGUMENT_DELIMITER"])
-    successful = self.coreReplace(searchTerm, replacementTerm, quietly=quietly)
+    successful, self.dataTape = coreReplace(self.dataTape, searchTerm, replacementTerm)
     #if not quietly:
     #  print("doCycle: successJumpText={}, failureJumpText={}, successful={}.".format(successJumpText, failureJumpText, successful))
-    jumpLocation = int(successJumpText if else failureJumpText, 2)
+    jumpLocation = int(successJumpText if successful else failureJumpText, 2)
     self.location = jumpLocation
     
   def printState(self):
-    #print("/----")
     print("  /--------------------------------\n  |\n  |{}\n  |\n  |{}\n  |\n  \\-".format(formatCompiledLine(self.location, self.splitProgramTape[self.location]),str(self.dataTape)))
-    #print("\\----")
     
   def run(self):
     self.printState()
